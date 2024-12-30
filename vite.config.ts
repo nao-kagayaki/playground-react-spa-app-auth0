f9c047a86ecbd339c9b01a79a0ae10ffe98f7ca3
nao-kagayaki <nyamada43@gmail.com>
2024-12-30 13:54:08 +0900

    clientディレクトリへの移動
    - src/clientディレクトリにプロジェクトを移動しました。
      - 理由: プロジェクト構造を整理し、クライアントサイドのコードを明確に分離するため。
      - 結果: コードの可読性とメンテナンス性が向上しました。
    
    Vite設定の変更: envDirオプションの追加
    
    - envDirオプションを追加し、共通の.envファイルをルートディレクトリに配置しました。
      - 理由: 環境変数の管理を一元化するため。
      - 結果: 環境変数の管理が容易になりました。
    
    Vite設定の変更: buildオプションの変更
    
    - buildオプションを変更し、ビルド出力先をdist/clientディレクトリに設定しました。
      - 理由: ビルド出力先を整理するため。
      - 結果: ビルド出力が整理されました。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: "src/client",
+  // TIP MEMO
+  // .envは共通でルートディレクトリに配置したい場合にはenvDirを指定する
+  // https://vite.dev/config/shared-options.html#envdir
+  envDir: "../..",
+  build: { 
+    outDir: "../../dist/client",
+  },
+});
